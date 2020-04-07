08eeaff1ac11527867846f8a2a0ad81223e240df
William <billy.ryan.rom@gmail.com>
2020-04-07 01:19:30 -0400

    Remove unnecessary components from base template * Add components folder * Add Header and Footer components to App.js

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div>
+            <h6>William Ryan © {(new Date().getFullYear())}</h6>
+        </div>
+    )
+}
+
+export default Footer
---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+    <div>
+        <nav>
+            <h3>Home</h3>
+            <h3>Projects</h3>
+            <h3>About</h3>
+            <h3>Contact</h3>
+        </nav>
+    </div>
+    )
+}
+
+export default Header;
