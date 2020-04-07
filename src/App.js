df0df2f4f9aa3cb270d3cd42a288c9e81e1bcbde
William <billy.ryan.rom@gmail.com>
2020-04-07 01:57:34 -0400

    Add Home component for landing page * Add images folder for internal images

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 
 import Header from './components/Header.js';
-import Footer from './components/Footer.js';
+import Footer from './components/Footer.js'
+
+import Home from './components/Home.js'
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Home/>
       <Footer/>
     </div>
   );
---
+++ src/components/Home.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Welcome</h1>
+            <h3>And</h3>
+            <h1>Congratulations!</h1>
+            <p>You've just found your next... Full-Stack Web Developer!</p>
+            <div>
+                <span role="img" aria-label="Right Arrow" >▶️</span>
+                <img src={require('../images/Professional.jpg')} alt="site owner"></img>
+                <span role="img" aria-label="Left Arrow" >◀️</span>
+            </div>
+        </div>
+    )
+}
+
+export default Home
