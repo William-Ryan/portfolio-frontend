e3b8e07102d901d28eac68d51958da2e95fa0ce0
William <billy.ryan.rom@gmail.com>
2020-04-07 02:06:52 -0400

    Add BrowserRouter and Provider to index.js * Add createStore and applyMiddleware to index.js * Add store and place thunk inside of it

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import {BrowserRouter as Router} from "react-router-dom"
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+
+import App from './App'
+
+const store = createStore(applyMiddleware(thunk));
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Provider store={store}>
+    <Router>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+      </Router>
+  </Provider>, document.getElementById('root')
 );
