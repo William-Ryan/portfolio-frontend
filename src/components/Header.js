3a7952672cf9dad435be0b7aaed046286fbce204
William <billy.ryan.rom@gmail.com>
2020-04-10 02:43:50 -0400

    Add material-ui to project * Add styling to header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,108 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Header = () => {
-    return (
-    <div>
-        <nav>
-            <h3><Link to="/">Home</Link></h3>
-            <h3><Link to="/projects">Projects</Link></h3>
-            <h3><Link to="/about">About</Link></h3>
-            <h3><Link to="/contact">Contact</Link></h3>
-        </nav>
-    </div>
-    )
-}
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import ClickAwayListener from '@material-ui/core/ClickAwayListener';
+import Grow from '@material-ui/core/Grow';
+import Paper from '@material-ui/core/Paper';
+import Popper from '@material-ui/core/Popper';
+import MenuItem from '@material-ui/core/MenuItem';
+import MenuList from '@material-ui/core/MenuList';
 
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    menuButton: {
+        marginRight: theme.spacing(2),
+    },
+    title: {
+        flexGrow: 1,
+    },
+    paper: {
+        marginRight: theme.spacing(2),
+    },
+}));
+
+const Header = () => {
+    const classes = useStyles();
+    const [open, setOpen] = React.useState(false);
+    const anchorRef = React.useRef(null);
+  
+    const handleToggle = () => {
+        setOpen((prevOpen) => !prevOpen);
+    };
+  
+    const handleClose = (event) => {
+        if (anchorRef.current && anchorRef.current.contains(event.target)) {
+          return;
+        }
+    
+        setOpen(false);
+      };
+
+      function handleListKeyDown(event) {
+        if (event.key === 'Tab') {
+          event.preventDefault();
+          setOpen(false);
+        }
+      }
+
+      const prevOpen = React.useRef(open);
+  React.useEffect(() => {
+    if (prevOpen.current === true && open === false) {
+      anchorRef.current.focus();
+    }
+
+    prevOpen.current = open;
+  }, [open]);
+  
+    return (
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton edge="start" 
+                className={classes.menuButton} 
+                color="inherit" 
+                aria-label="menu"
+                ref={anchorRef}
+                aria-controls={open ? 'menu-list-grow' : undefined}
+                aria-haspopup="true"
+                onClick={handleToggle}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Popper open={open} anchorEl={anchorRef.current} role={undefined} transition disablePortal>
+          {({ TransitionProps, placement }) => (
+            <Grow
+              {...TransitionProps}
+              style={{ transformOrigin: placement === 'bottom' ? 'center top' : 'center bottom' }}
+            >
+              <Paper>
+                <ClickAwayListener onClickAway={handleClose}>
+                  <MenuList autoFocusItem={open} id="menu-list-grow" onKeyDown={handleListKeyDown}>
+                    <MenuItem onClick={handleClose}><Link to="/" style={{ textDecoration: 'none', color: 'black'}}>Home</Link></MenuItem>
+                    <MenuItem onClick={handleClose}><Link to="/projects" style={{ textDecoration: 'none', color: 'black'}}>Projects</Link></MenuItem>
+                    <MenuItem onClick={handleClose}><Link to="/about" style={{ textDecoration: 'none', color: 'black'}}>About</Link></MenuItem>
+                    <MenuItem onClick={handleClose}><Link to="/contact" style={{ textDecoration: 'none', color: 'black'}}>Contact</Link></MenuItem>
+                  </MenuList>
+                </ClickAwayListener>
+              </Paper>
+            </Grow>
+          )}
+        </Popper>
+            <Typography variant="h6" className={classes.title}>
+              William's Portfolio
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </div>
+    );
+}
 export default Header;
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -8,7 +8,7 @@ const ProjectList = props => {
 
     useEffect(() => {
         props.fetchProjects()
-    }, [])
+    }, [props])
 
     return (
         <div>
