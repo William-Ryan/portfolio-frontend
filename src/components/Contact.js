a39e63070be018c98d953d976fa6980f54271a0b
William <billy.ryan.rom@gmail.com>
2020-04-15 06:19:19 -0400

    Add styling to contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,17 +1,38 @@
-import React from 'react'
+import React from 'react';
+
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles({
+    root: {
+        maxWidth: '20%',
+        padding: '4% 0%',
+        margin: '12% auto',
+        fontSize: '1.3rem',
+        lineHeight: '3rem'
+    },
+})
 
 const Contact = () => {
+    const classes = useStyles();
+
     return (
-        <div>
-            <h1>Contact Me</h1>
-            <address>
-                <ul>
-                    <li>Location: Martinsburg, WV</li>
-                    <li>Email: billy.ryan.rom@gmail.com</li>
-                    <li>Message my <a href="https://www.linkedin.com/in/william-ryan-34a1981a1/">LinkedIn Account</a></li>
-                </ul>
-            </address>
-        </div>
+        <Card className={classes.root}>
+                <CardMedia />
+                <CardContent>
+                <Typography gutterBottom variant="h5" style={{ textAlign: 'center' }}>Contact Me</Typography>
+                <address>
+                    <ul>
+                        <li>Location: Martinsburg, WV</li>
+                        <li>Email: billy.ryan.rom@gmail.com</li>
+                        <li>Message my <a href="https://www.linkedin.com/in/william-ryan-34a1981a1/">LinkedIn Account</a></li>
+                    </ul>
+                </address>
+            </CardContent>
+        </Card>
     )
 }
 
