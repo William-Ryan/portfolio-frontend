b1396694affcf6777ea7cfa4ab2404423e6f8851
William <billy.ryan.rom@gmail.com>
2020-04-15 04:31:27 -0400

    Add further detail to about page including... * A personal summary * Developer and Personal Skills * Closing Statement

--- src/components/About.js
+++ src/components/About.js
@@ -4,10 +4,59 @@ const About = () => {
     return (
         <div>
             <h1>About Me</h1>
-            <h3>Who is William Ryan?</h3>
-            <p>Hi, I'm a full-stack developer!</p>
-            <h3>My Skills</h3>
-            <p>React, Redux, NodeJS, Express, etc</p>
+            <div>
+                <h2>Who is William Ryan?</h2>
+                <p>I'm first and foremost a lover of technology and learning who's ambition and determination 
+                    allows them to tackle any problem head on and find the solution! My technical profession is 
+                    being a Full-Stack Developer, but truthfully I feel my education and experiences have given 
+                    me so much more then the average Developer. I am confident with time I can take just about 
+                    any language or library and gain an aptitude for it's knowledge. I look forward to getting to 
+                    know you better and perhaps even working together. Thank you!</p>
+            </div>
+            <div>
+                <h2>My Developer Skills</h2>
+                <h3>Front-End</h3>
+                <ul>
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <li>LESS</li>
+                    <li>JavaScript</li>
+                    <li>React</li>
+                    <li>Redux</li>
+                    <li>Context API</li>
+                    <li>Styled-Components</li>
+                    <li>BootStrap</li>
+                    <li>ReactStrap</li>
+                    <li>Material-UI</li>
+                </ul>
+                <h3>Back-End</h3>
+                <ul>
+                    <li>JavaScript</li>
+                    <li>NODE.JS</li>
+                    <li>Express</li>
+                    <li>Knex</li>
+                    <li>Sqlite3</li>
+                    <li>Postgres</li>
+                    <li>Postman</li>
+                </ul>
+                <h3>Personal Ability</h3>
+                <ul>
+                    <li>Leadership</li>
+                    <li>Mentoring</li>
+                    <li>Organized</li>
+                    <li>Team-Oriented</li>
+                    <li>Independently Driven</li>
+                    <li>Strives for Perfection</li>
+                    <li>Innovative</li>
+                </ul>
+            </div>
+            <div>
+                <p>The technical skills I've listed here I feel confident in the ins and outs of their nature,
+                    such as the use of custom hooks, parameters, and other nuances that they may be capable of.
+                    This list is open and likely in this very moment is being expanded and explored so feel
+                    free to inquire on any interests in fields that have not been directly stated above.
+                </p>
+            </div>
         </div>
     )
 }
