3ebd516381697cc6301ef6153fceae107f396682
William <billy.ryan.rom@gmail.com>
2020-04-17 17:09:19 -0400

    Remove Node.js database for backend * Refractor About.js and Update Firebase

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,77 +1,50 @@
 import React, { useState, useEffect } from "react";
 
 import firebase from '../firebase.js'
-// import Projects from "./Projects"
-// import { connect } from "react-redux";
-
-// import { fetchProjects } from "../actions";
-
-// const ProjectList = props => {
-
-//     useEffect(() => {
-//         props.fetchProjects()
-//     }, [props])
-
-//     return (
-//         <div>
-//             <h1 className="title">Projects Showcase</h1>
-//             <div className = "card-container">
-//                 {props.projects.map(project => (
-//                     <Projects project={project}/>
-//                 ))}
-//             </div>
-//         </div>
-//     )
-// }
-
-// const mapStateToProps = state => {
-//     return {
-//         projects: state.projects,
-//         isFetching: state.isFetching,
-//         errors: state.errors
-//     }
-// }
-
-// export default connect(
-//     mapStateToProps, 
-//     { fetchProjects }
-// )(ProjectList);
 
 function Portfolio(){
-    const [projects, setProjects] = useState([])
+    const [backendProjects, setBackendProjects] = useState([])
 
     useEffect(() => {
         firebase
             .firestore()
             .collection('Portfolio')
             .onSnapshot((snapshot) => {
-                const newProjects = snapshot.docs.map((doc) => ({
+                const newBackendProjects = snapshot.docs.map((doc) => ({
                     id: doc.id,
                     ...doc.data()
                 }))
 
-                setProjects(newProjects)
+                setBackendProjects(newBackendProjects)
             })
     }, [])
 
-    return projects
+    return backendProjects
 }
 
 const ProjectList = () => {
-    const projects = Portfolio()
+    const backendProjects = Portfolio()
 
     return (
         <div>
             <h1 className="title">Projects Showcase</h1>
             <div>
-                {projects.map((project) => 
-                    <div key={project.Projects}>
+                <h2>Back-End Projects</h2>
+                {backendProjects.map((project) => 
+                    <div key={project.id}>
                     <h3>{project.Title}</h3>
                     <p>{project.Description}</p>
-                    <h4>{project.Skills}</h4>
-                    <a href={project.GitHub}>GitHub Link</a>
+                    {project.Tech.map((tech) => 
+                        <ul key={tech.id}>
+                            <li>{tech}</li>
+                        </ul>
+                    )}
+                    <button><a href={project.GitHub}>GitHub Link</a></button>
                     </div>
                 )}
+            </div>
+            <div>
+                    <h2>Front-End Projects</h2>
             </div>
         </div>
     )
--- src/index.js
+++ src/index.js
@@ -1,21 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from "react-router-dom"
-import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
-import thunk from "redux-thunk";
 
-import Reducer from './reducer/index.js'
 import App from './App'
 
-const store = createStore(Reducer, applyMiddleware(thunk));
 
 ReactDOM.render(
-  <Provider store={store}>
-    <Router>
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
-    </Router>
-  </Provider>, document.getElementById('root')
+  <Router>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Router>, document.getElementById('root')
 );
